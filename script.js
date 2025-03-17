751a78654c83fb5413f0ecbd154845cf8ff652d2
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-03-17 17:38:06 -0300

    Inicializando Front-End da aplicacao

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function shortenUrl() {
-    const url = document.getElementById("urlInput").value; // Agora "url" está definida!
+    const url = document.getElementById("urlInput").value; 
 
     fetch("http://localhost:8080/shorten-url", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ url: url }) // Agora funciona!
+        body: JSON.stringify({ url: url }) 
     })
     .then(response => response.json())
     .then(data => {
-        console.log("Resposta da API:", data); // Debug no console
+        console.log("Resposta da API:", data); 
         document.getElementById("shortUrl").href = data.url;
         document.getElementById("shortUrl").innerText = data.url;
         document.getElementById("result").style.display = "block";
